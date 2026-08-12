881696b6252d26bfb70bc4ff0b38c0d18ab5c870
ragul <manwaysoftware@gmail.com>
2026-08-12 14:12:12 +0530

    cource created

--- app.ts
+++ app.ts
@@ -1,16 +1,13 @@
 import exp from 'express';
 import { error } from 'node:console';
 import { server,environment } from './config.js';
+import { CourseController, CourseManagerService, SQLiteCourseDAL, createCourseRouter } from './course.js';
 const app: exp.Application = exp();
 
 //add this first line
 app.use(exp.json())
-
-app.get('/', (req: exp.Request, res: exp.Response) => {
     
-res.status(200).json("hello")
-    
-})
+app.use("/courses", createCourseRouter(new CourseController(new CourseManagerService(new SQLiteCourseDAL()))));
 
 app.listen(server.port, () => {
     console.log('Server is running on http://localhost:'+JSON.stringify(server)+"environement:"+environment);
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Lib
+1) sqllite,sqllite3->npm install sqlite3 @types/sqlite3
