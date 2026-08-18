38cf15fbd077e68d7d1b06afe5db631d5b537a30
ragul <manwaysoftware@gmail.com>
2026-08-18 22:02:38 +0530

    cors add

--- app.ts
+++ app.ts
@@ -10,6 +10,18 @@ const app: exp.Application = exp();
 
 app.use(exp.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Shop-Domain');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    return;
+  }
+  next();
+});
+
 
 app.use('/courses', courseRouter);
 app.use('/student', studentRouter);
